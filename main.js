3bf65086a711e3ad671aa6ed396a4d217eecc67b
suburrrr <ainurrahmanhdyt@gmail.com>
2024-08-17 20:03:47 +0700

    update

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ function read_Data(index_sheet) {
   return data_sheet;
 }
 
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
